328b1d8ef69d745c77ca2a7575755af734a2cddf
Eva Aggarwal <eva.aggarwal@duke.edu>
2024-08-12 00:57:25 +0530

    added pause animation and logic

--- vinyl-animation/src/AlternateVinyl.js
+++ vinyl-animation/src/AlternateVinyl.js
@@ -2,18 +2,33 @@ import React, {useState} from 'react'
 
 const AlternateVinyl = () => {
 
-    const[isPlaying, setIsPlaying] = useState(true);
+    const[isPlaying, setIsPlaying] = useState(false);
+    const[wasPlaying, setWasPlaying] = useState(false);
 
-    const handeClick = () => {
+    const handleClick = () => {
+        setWasPlaying(isPlaying);
         setIsPlaying(!isPlaying);
+    }
+
+    function animationState(isPlaying, wasPlaying){
+        if (isPlaying == wasPlaying){
+            return '-stationary';
+        }
+        else if(isPlaying){
+            return '-playing';
+        }
+        else{
+            return '-paused';
+        }
+
     }
 
   return (
     <div>
     <div className='vinyl-container'>
         <div className='vinyl-turntable'>
-            <img className={`vinyl-record${isPlaying ? '-paused' : ''}`} src='/plain-vinyl-square.png' alt='vinyl record' />
-            <img className={`vinyl-stylus${isPlaying ? '' : '-playing'}`} src='/vinyl-stylus-side.png' alt='vinyl stylus' onClick={handeClick} />
+            <img className={`vinyl-record${animationState(isPlaying, wasPlaying)}`} src='/plain-vinyl-square.png' alt='vinyl record' />
+            <img className={`vinyl-stylus${animationState(isPlaying, wasPlaying)}`} src='/vinyl-stylus-side.png' alt='vinyl stylus' onClick={handleClick} />
         </div>
     </div>
 </div>
